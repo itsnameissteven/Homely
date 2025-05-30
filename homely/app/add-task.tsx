20f842651b1616eb67bc7edd7aa19a969ca6edde
Steven <stevenrmancine@gmail.com>
2025-05-29 20:45:50 -0600

    Clean

--- homely/app/add-task.tsx
+++ homely/app/add-task.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const FORM = { name: '' }
+const FORM = { name: '', description: "" };
 export default function AddTask() {
   const [form, setForm] = useState(FORM)
   return (
@@ -12,7 +12,7 @@ export default function AddTask() {
     <SafeAreaView style={commonStyles.mainView}>
       <AppText style={styles.text}>Add a New Task</AppText>
       <View style={styles.textContainer}>
-        <AppText>Task Name</AppText>
+        <AppText style={styles.label}>Task Name</AppText>
         <TextInput
           style={styles.input}
           onChangeText={(text) => setForm(prev => {
@@ -26,6 +26,25 @@ export default function AddTask() {
           placeholderTextColor={"#fff"}
         />
       </View>
+      <View style={styles.textContainer}>
+        <AppText style={styles.label}>Description</AppText>
+        <TextInput
+          editable
+          multiline
+          numberOfLines={4}
+          maxLength={40}
+          placeholder="Description"
+          placeholderTextColor={"#fff"}
+          onChangeText={text => setForm(prev => {
+            return {
+              ...prev,
+              description: text
+            }
+          })}
+          value={form.description}
+          style={styles.textInput}
+        />
+      </View>
     </SafeAreaView>
   )
 }
@@ -34,10 +53,13 @@ const styles = StyleSheet.create({
   textContainer: {
     marginTop: 20
   },
+  label: {
+    marginStart: 4,
+  },
   text: {
-    color: '#fff',
+
     fontSize: 24,
-    fontWeight: "bold"
+
   },
   input: {
     height: 40,
@@ -47,5 +69,15 @@ const styles = StyleSheet.create({
     color: "#fff",
     borderColor: "#fff",
     padding: 10,
+  },
+  textInput: {
+    padding: 10,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 4,
+    color: "#fff",
+    borderColor: "#fff",
+    height: 100,
+    textAlignVertical: "top",
   },
 });
--- homely/components/appText.tsx
+++ homely/components/appText.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-native";
 
 export function AppText(props: TextProps) {
-  return <Text style={styles.text} {...props}>{props.children}</Text>
+  return <Text {...props} style={StyleSheet.flatten([styles.text, props.style])}>{props.children}</Text>
 }
 
 const styles = StyleSheet.create({
